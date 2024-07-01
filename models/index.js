70003663c28abb85a199a562096cced3d30ff17d
Muhammad Abduh <imuhammadabduh@gmail.com>
2024-07-01 23:03:44 +0700

    dialect modul mysql2

--- models/index.js
+++ models/index.js
@@ -1,7 +1,9 @@
 const { Sequelize } = require("sequelize");
+const mysql2 = require("mysql2");
 
 const sequelize = new Sequelize("sql12717194", "sql12717194", "7SjrpEnau1", {
   host: "sql12.freesqldatabase.com",
+  dialectModule: mysql2,
   dialect: "mysql",
 });
 
